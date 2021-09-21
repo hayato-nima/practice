2ef6f941fc098685ac58e7c0f603d22233103f9d
hayato-nima <kdc98bac72xkx@gmail.com>
2021-09-22 03:43:50 +0900

    update

--- paiza/code.php
+++ paiza/code.php
@@ -7,20 +7,44 @@
 
 
 // 【キャンペーン問題】枠で囲む
-
 // $title = 'asdfghjklfdadfafdfa';
-
 // $upper = mb_strlen($title);
-
 // $cross = ($upper + 2);
-
 // function enclose($val)
 // {
 //     for ($i = 1; $i < $val; $i++) {
 //         echo '+';
 //     }
 // };
-
 // echo (enclose($cross)) . "\n", "+{$title}+" . "\n", enclose($cross);
 
-?>
+
+// $n = trim(fgets(STDIN));//trimは文字列の最初と最後の空白を取り除く関数です。
+// $zaisan = [];
+
+// for ($i = 0; $i < $n; $i++) {
+//     [$s, $a] = explode(' ', trim(fgets(STDIN)));// explode関数とは、文字列を指定した文字列で分割する関数です。
+//     $zaisan[$s] = $a;
+// }
+
+// $S = trim(fgets(STDIN));
+
+// echo $zaisan[$S] . "\n";
+// $zaisan = []; とすることで $zaisan を初期化します。
+// $zaisan に $s に対応する値 $a を追加するには $zaisan[$s] = $a; とします。
+// $zaisan における $S に対応する値にアクセスするには $zaisan[$S] とします。
+
+// C020
+// $zanpan = '10 31 52';
+// $nakami = explode(' ', $zanpan);
+// $m = $nakami[0];
+// $p = $nakami[1] / 100;
+// $q = $nakami[2] / 100;
+// $hanbai1 = $m * $p;
+// $souzai = $m - $hanbai1;
+// $hanbai2 = $souzai * $q;
+// $nokori = $souzai - $hanbai2;
+// echo $nokori;
+
+
+// var_dump($nakami) ;
